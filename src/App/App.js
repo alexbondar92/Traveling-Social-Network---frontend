63b16ed0c4aea54e50bf903a12ed0bb99f741f93
Alex Bondar <Alex.Bondar@campus.technion.ac.il>
2019-12-29 13:59:06 +0100

    basic login & register modules(without connection to backend)

---
+++ src/App/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+// import { connect } from 'react-redux';
+
+import logo from './logo.svg';
+import './App.css';
+
+import { HomeModule } from '../HomeModule';
+import { LoginModule } from '../LoginModule';
+import { RegisterModule } from '../RegisterModule';
+import {history} from '../_helpers';
+import {PrivateRoute} from '../_helpers';
+
+function App_old() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+
+      history.listen((location, action) => {
+          // clear alert on location change
+          this.props.clearAlerts();
+      });
+  }
+
+  render() {
+    return (
+      <Router history={history}>
+        <div>
+          <Route exact path="/" component={App_old} />
+          <Route exact path="/login" component={LoginModule} />
+          <Route exact path="/register" component={RegisterModule} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/RegisterModule/RegisterModule.js
@@ -0,0 +1,89 @@
+import React from 'react';
+
+
+class RegisterModule extends React.Component {
+	constructor(props) {
+        super(props);
+
+        this.state = {
+        	first_name: '',
+        	last_name: '',
+        	email: '',
+            username: '',
+            password: '',
+            submitted: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+  	handleSubmit(event) {
+    	event.preventDefault();
+
+    	this.setState({ submitted: true });
+
+    	// TODO: validation...
+    	// TODO: send async http request to backend 
+  	}
+
+  	render () {
+		const { loggingIn } = this.props;
+        const { first_name, last_name, email, username, password, submitted } = this.state;
+		return (
+            <div className="col-md-6 col-md-offset-3">
+                <h2>Register</h2>
+                <form name="form" onSubmit={this.handleSubmit}>
+                    <div className={'form-group' + (submitted && !first_name ? ' has-error' : '')}>
+                        <label htmlFor="first_name">First Name</label>
+                        <input type="text" className="form-control" name="first_name" value={first_name} onChange={this.handleChange} />
+                        {submitted && !first_name &&
+                            <div className="help-block">First Name is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !last_name ? ' has-error' : '')}>
+                        <label htmlFor="last_name">Last Name</label>
+                        <input type="text" className="form-control" name="last_name" value={last_name} onChange={this.handleChange} />
+                        {submitted && !last_name &&
+                            <div className="help-block">Last Name is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
+                        {submitted && !username &&
+                            <div className="help-block">Username is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !email ? ' has-error' : '')}>
+                        <label htmlFor="email">Email</label>
+                        <input type="text" className="form-control" name="email" value={email} onChange={this.handleChange} />
+                        {submitted && !email &&
+                            <div className="help-block">Email is required</div>
+                        }
+                    </div>                    
+                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
+                        {submitted && !password &&
+                            <div className="help-block">Password is required</div>
+                        }
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary">Register</button>
+                        {loggingIn &&
+                            <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                        }
+                    </div>
+                </form>
+            </div>
+		);
+	}
+}
+
+export { RegisterModule };
